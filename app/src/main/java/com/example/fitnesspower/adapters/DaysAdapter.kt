3ed734c47d64842ae4cd2e91e67b8b329f0c5e7b
DmitrySlv <dscreateapp@gmail.com>
2023-01-11 14:34:17 +0400

    Создание DaysAdapter.kt ч2. Добавление класса DiffUtilDaysAdapter.kt для ListAdapter.

--- app/src/main/java/com/example/fitnesspower/adapters/DaysAdapter.kt
+++ app/src/main/java/com/example/fitnesspower/adapters/DaysAdapter.kt
@@ -1,14 +1,25 @@
 package com.example.fitnesspower.adapters
 
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
-import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.example.fitnesspower.R
 import com.example.fitnesspower.databinding.DaysListItemBinding
 import com.example.fitnesspower.models.DayModel
 
-class DaysAdapter: ListAdapter<DayModel, DaysAdapter.DayHolder>() {
+class DaysAdapter: ListAdapter<DayModel, DaysAdapter.DayHolder>(DiffUtilDaysAdapter) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DayHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.days_list_item,parent,false)
+        return DayHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: DayHolder, position: Int) {
+        holder.setData(getItem(position))
+    }
 
     class DayHolder(view: View): ViewHolder(view) {
         private val binding = DaysListItemBinding.bind(view)
